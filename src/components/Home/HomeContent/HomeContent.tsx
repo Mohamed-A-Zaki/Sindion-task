ebf8367bbce033e62696280f09ffb8c04d0d1a9e
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2023-09-25 16:04:31 +0300

    upload

--- src/components/Home/HomeContent/HomeContent.tsx
+++ src/components/Home/HomeContent/HomeContent.tsx
@@ -1,18 +1,9 @@
-import { useEffect } from "react";
-import { useAppDispatch } from "../../../store/hooks";
 import NoDataToShow from "../NoDataToShow";
 import TicketsTable from "../TicketsTable";
-import { setTicketList } from "../../../store/ticketSlice";
 import useGetAllTickets from "../../../hooks/useGetAllTickets";
-import { TicketType } from "../../../types/tickets";
 
 export default function HomeContent() {
-  const dispath = useAppDispatch();
   const { data, isLoading, isError } = useGetAllTickets();
-
-  useEffect(() => {
-    dispath(setTicketList(data?.data as TicketType[]));
-  }, [data?.data, dispath]);
 
   if (isLoading) {
     return <div>Loading...</div>;
--- src/hooks/useGetAllTickets.tsx
+++ src/hooks/useGetAllTickets.tsx
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
 import { useQuery } from "react-query";
 import { BaseURL } from "../api/BaseURL";
 import { TicketType } from "../types/tickets";
+import { useAppDispatch } from "../store/hooks";
+import { setTicketList } from "../store/ticketSlice";
 
 export default function useGetAllTickets() {
+  const dispatch = useAppDispatch();
+
   const { data, isLoading, isError } = useQuery("tickets", () => {
     return BaseURL.get<TicketType[]>("tickets");
   });
+
+  useEffect(() => {
+    dispatch(setTicketList(data?.data as TicketType[]));
+  }, [data?.data, dispatch]);
 
   return { data, isLoading, isError };
 }
